83a2f67ebe07dbd11b13a23e905bb8a843c49d5e
Saranya Nambiar <64544937+saranyanambiarr@users.noreply.github.com>
2020-07-12 16:42:01 +0530

    Update failed_mail.py

--- failed_mail.py
+++ failed_mail.py
@@ -5,11 +5,11 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("sharanya8095@gmail.com", "lEgAcY123_21") 
+s.login("sender_mail", "your_pwd") 
 # message to be sent 
 message = "Hey Developer, you need to check your code once. The accuracy of your model is not so good."
 # sending the mail 
-s.sendmail("sharanya8095@gmail.com", "nambiar.saranya98@gmail.com", message) 
+s.sendmail("sender_mail", "developer_mail", message) 
 # terminating the session 
 print("Mail has been sent to developer.")
 s.quit()
