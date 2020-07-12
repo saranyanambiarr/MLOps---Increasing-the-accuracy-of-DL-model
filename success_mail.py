44e71e4f8cf4c21c4294ad32c86c9ee744edb79b
Saranya Nambiar <64544937+saranyanambiarr@users.noreply.github.com>
2020-07-12 16:40:44 +0530

    Update success_mail.py

--- success_mail.py
+++ success_mail.py
@@ -5,11 +5,11 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("sharanya8095@gmail.com", "lEgAcY123_21") 
+s.login("sender_mail", "yourpwd") 
 # message to be sent 
 message = "Hey Developer, your model is trained and giving good accuracy!"
 # sending the mail 
-s.sendmail("sharanya8095@gmail.com", "nambiar.saranya98@gmail.com", message) 
+s.sendmail("sender_mail", "developer_mail", message) 
 # terminating the session 
 print("Mail has been sent to developer.")
 s.quit()
